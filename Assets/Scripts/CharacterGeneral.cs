13437e32df620116281338b5d365ffa649a9f43a
DaeHyun Kang <limitedcopy@gmail.com>
2018-03-24 17:04:26 +0900

    Add Player Movement, Rotate Aim

--- Assets/Scripts/CharacterGeneral.cs
+++ Assets/Scripts/CharacterGeneral.cs
@@ -2,16 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterGeneral : MonoBehaviour {
+public abstract class CharacterGeneral : MonoBehaviour {
+
+    public int n_hp;
+
+    public float f_AimDegree;
+    public float f_Speed;
+    public bool b_Ranged;
+    public string s_Weapon;
+    public Transform g_Sprite;
+    public Transform g_Weapon;
+    public float f_SpritelocalScale;
+    public float f_WeaponlocalScale;
+
+    protected void GetAimDegree(Vector3 v_TargetPos)
+    {
+        
+        float x = g_Weapon.position.x - v_TargetPos.x;
+        float y = g_Weapon.position.y - v_TargetPos.y;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        f_AimDegree = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+    }
+    
+    public virtual void InitializeParam()
+    {
+        g_Sprite = transform.Find("Sprite");
+        if (b_Ranged)
+        {
+            g_Weapon = transform.Find("Weapon");
+        }
+        f_SpritelocalScale = g_Sprite.localScale.x;
+        f_WeaponlocalScale = g_Sprite.localScale.y;
+    }
+
+
+    public virtual void CharacterMovement()
+    {
+
+    }
+
+
+    public virtual void RotateGun(Vector3 v_TargetPos)
+    {
+
+        GetAimDegree(v_TargetPos);
+        g_Weapon.rotation = Quaternion.Euler(new Vector3(0, 0, f_AimDegree));
+
+        if (f_AimDegree > -90 && f_AimDegree <= 90)
+        {
+            g_Sprite.localScale = new Vector3(f_SpritelocalScale, g_Sprite.localScale.y, g_Sprite.localScale.z);
+            g_Weapon.localScale = new Vector3(g_Weapon.localScale.x, f_WeaponlocalScale, g_Weapon.localScale.z);
+        }
+        else
+        {
+            g_Sprite.localScale = new Vector3(-f_SpritelocalScale, g_Sprite.localScale.y, g_Sprite.localScale.z);
+            g_Weapon.localScale = new Vector3(g_Weapon.localScale.x, -f_WeaponlocalScale, g_Weapon.localScale.z);
+        }
+    }
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,16 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : CharacterGeneral {
 
-    
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    bool b_LeftorRight = false;
+    bool b_UporDown = false;
+
+    // Use this for initialization
+    void Start () {
+        InitializeParam();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        Vector3 v_MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RotateGun(v_MousePos);
+        CharacterMovement();
 	}
+
+    
+
+    public override void CharacterMovement()
+    {
+        float f_DeltaSpeed = f_Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-f_DeltaSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(f_DeltaSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, -f_DeltaSpeed));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, f_DeltaSpeed));
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
