c84a9a192113e8097a2df4b5890ee2dc7264dfee
Yoohogyun <yoohoogun114@naver.com>
2018-03-28 18:36:55 +0900

    stash room

---
+++ Assets/Prefabs/UI/SampleBtn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleBtn : MonoBehaviour {
+	public Button button;
+	public Text roomName;
+	public Text roomState;
+
+	private Item item;
+    private RoomScrollList roomList;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void Setup(Item currentItem, RoomScrollList currentScrollList)
+    {
+        item = currentItem;
+        roomName.text = item.roomName;
+        roomState.text = item.roomState;
+        roomList = currentScrollList;
+        
+    }
+    
+    public void HandleClick()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RoomList/RoomScrollList.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Item {
+	public string roomName;
+	public string roomState;
+}
+
+public class RoomScrollList : MonoBehaviour {
+	public List<Item> roomList;
+	public Transform contentPanel;
+	public RoomObjectPool roomObjectPool;
+
+
+	// Use this for initialization
+	void Start () {
+		RefreshDisplay();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// AddButtons
+	private void AddRooms() {
+		for(int i = 0; i < roomList.Count; i++) {
+			Item item = roomList[i];
+			GameObject newButton = roomObjectPool.GetObject();
+			newButton.transform.SetParent(contentPanel);
+			SampleBtn sampleBtn = newButton.GetComponent<SampleBtn>();
+			sampleBtn.Setup(item, this);
+		}
+	}
+
+	void RefreshDisplay() {
+        RemoveButtons ();
+        AddButtons ();
+    }
+
+	private void RemoveButtons()
+    {
+        while (contentPanel.childCount > 0) 
+        {
+            GameObject toRemove = transform.GetChild(0).gameObject;
+            roomObjectPool.ReturnObject(toRemove);
+        }
+    }
+
+	private void AddButtons()
+    {
+        for (int i = 0; i < roomList.Count; i++) 
+        {
+            Item item = roomList[i];
+            GameObject newButton = roomObjectPool.GetObject();
+            newButton.transform.SetParent(contentPanel);
+
+            SampleBtn sampleButton = newButton.GetComponent<SampleBtn>();
+            sampleButton.Setup(item, this);
+        }
+    }
+
+	void AddItem(Item itemToAdd, RoomScrollList roomScrollList)
+    {
+        roomScrollList.roomList.Add (itemToAdd);
+    }
+
+    private void RemoveItem(Item itemToRemove, RoomScrollList roomScrollList)
+    {
+        for (int i = roomScrollList.roomList.Count - 1; i >= 0; i--) 
+        {
+            if (roomScrollList.roomList[i] == itemToRemove)
+            {
+                roomScrollList.roomList.RemoveAt(i);
+            }
+        }
+    }
+}
