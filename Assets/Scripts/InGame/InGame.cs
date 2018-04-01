b43b64a3fb7a2d44a067a13823776d4408ab1cdc
Yoohogyun <yoohoogun114@naver.com>
2018-04-01 12:39:55 +0900

    Error on instanciating

--- Assets/Scripts/InGame/InGame.cs
+++ Assets/Scripts/InGame/InGame.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class InGame : MonoBehaviour {
 
-	public GameObject playerPrefab;
-
 	// Use this for initialization
 	void Start () {
-		PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+		PhotonNetwork.Instantiate("Attacker", new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
 	}
 	
 	// Update is called once per frame
