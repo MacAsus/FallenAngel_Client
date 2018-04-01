1a268157ee491cb803628fdea747e59f35e98d05
Yoohogyun <yoohoogun114@naver.com>
2018-04-01 12:30:52 +0900

    Photon Instanciating

---
+++ Assets/Scripts/InGame/InGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGame : MonoBehaviour {
+
+	public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
