7124e753804e114ff03cffd2f9368e71bbe810c9
Yoohogyun <yoohoogun114@naver.com>
2018-04-10 10:02:01 +0900

    Synchronize Player Movement Smooth

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,6 +24,11 @@ public class Player : CharacterGeneral
 
     Vector3 v_MousePos = new Vector3();
 
+    // for sending Value
+
+    // by PhotonNetwork Value
+    Vector3 v_NetworkPosition;
+    double f_LastNetworkDataReceivedTime;
     // Use this for initialization
     void Start()
     {
@@ -35,6 +40,8 @@ public class Player : CharacterGeneral
         //여기까지
         PhotonNetwork.sendRate = 500 / Launcher.MaxPlayersPerRoom;
         PhotonNetwork.sendRateOnSerialize = 500 / Launcher.MaxPlayersPerRoom;
+        PhotonNetwork.sendRate = 500;
+        PhotonNetwork.sendRateOnSerialize = 500;
         InitializeParam();
 
         //디버그용
@@ -58,29 +65,46 @@ public class Player : CharacterGeneral
     void Update()
     {
         // if this view is not mine, then do not update
-        if (photonView.isMine == false && PhotonNetwork.connected == true)
+        if (photonView.isMine)
         {
-            return;
+            if (e_SpriteState != SpriteState.Dead)
+            {
+                e_SpriteState = SpriteState.Idle;
+
+                v_MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                // movement synced by Photon View
+                CharacterMovement();
+
+                // below value should be setted by manually
+
+                //스파인 애니메이션, 총알의 발사 모두 처리하는 함수
+                AnimationControl(e_SpriteState, b_Fired);
+                RotateGun(v_MousePos);
+                ChangeWeapon();
+            }
+        } else {
+            UpdateNetworkedPosition();
+        }
+    }
+
+    void UpdateNetworkedPosition() {
+        float pingInSeconds = (float)PhotonNetwork.GetPing() * 0.001f;
+		float timeSinceLastUpdate = (float)( PhotonNetwork.time - f_LastNetworkDataReceivedTime );
+		float totalTimePassed = pingInSeconds + timeSinceLastUpdate;
+        int lerpValue = 20; // lerpValue가 높아질 수록 빠르게 따라잡음
+
+        Vector3 newPosition = Vector3.Lerp(transform.position, v_NetworkPosition, Time.smoothDeltaTime * lerpValue); // 
+
+        if( Vector3.Distance( transform.position, v_NetworkPosition ) > 3f )
+		{
+			newPosition = v_NetworkPosition;
+            Debug.Log("Teleport");
         }
 
+        // Debug.Log("newPosition is" + newPosition.x + " : " + newPosition.y);
 
-        if (e_SpriteState != SpriteState.Dead)
-        {
-            e_SpriteState = SpriteState.Idle;
-
-            v_MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            // movement synced by Photon View
-            CharacterMovement();
-
-            // below value should be setted by manually
-
-            //스파인 애니메이션, 총알의 발사 모두 처리하는 함수
-            AnimationControl(e_SpriteState, b_Fired);
-            RotateGun(v_MousePos);
-            ChangeWeapon();
-        }
-
+        transform.position = newPosition;
     }
 
     //캐릭터 이동 
@@ -95,7 +119,7 @@ public class Player : CharacterGeneral
             {
                 tempx -= 1;
             }
-            if(Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.D))
             {
                 tempx += 1;
             }
@@ -107,11 +131,10 @@ public class Player : CharacterGeneral
             {
                 tempy -= 1;
             }
-
-
         }
-        rigid.velocity = new Vector2(f_Speed * tempx, f_Speed * tempy);
 
+        v_NetworkPosition = new Vector3(rigid.position.x + (f_Speed * tempx * Time.deltaTime), rigid.position.y + (f_Speed * tempy * Time.deltaTime));
+        rigid.velocity = new Vector2(f_Speed * tempx, f_Speed * tempy);
     }
 
     //스파인 이벤트 처리
@@ -156,21 +179,22 @@ public class Player : CharacterGeneral
     }
 
     [PunRPC]
-    void FireBulletNetwork(Vector3 muzzlePos, Vector3 bulletSpeed) {
+    void FireBulletNetwork(Vector3 muzzlePos, Vector3 bulletSpeed)
+    {
         Debug.Log("FireBulletNetwork called");
-        if(this.photonView.isMine)
+        if (this.photonView.isMine)
         {
             GameObject bullet = Instantiate(this.g_Bullet, muzzlePos, Quaternion.identity);
             BulletGeneral temp_bullet = bullet.GetComponent<BulletGeneral>();
             temp_bullet.bulletInfo = new GeneralInitialize.BulletParameter(gameObject.tag, cur_Weapon.f_Damage);
             bullet.GetComponent<Rigidbody2D>().velocity = (muzzlePos - this.g_Weapon.transform.position).normalized * this.cur_Weapon.f_BulletSpeed;
         }
-        
+
     }
 
     protected override void WeaponSpineControl(bool _b_Fired)
     {
-        
+
         if (Input.GetKey(KeyCode.Mouse0) && !_b_Fired) // triggerd by local
         {
             // GameObject bullet = weaponScript.FireBullet();
@@ -181,24 +205,26 @@ public class Player : CharacterGeneral
 
     void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
+        // Debug.Log("SerializeState Called");
         if (stream.isWriting)
         {
-            stream.SendNext(e_SpriteState);
-            stream.SendNext(b_Fired);
-            stream.SendNext(v_MousePos);
+            stream.SendNext(v_NetworkPosition); // 현재 위치가 아니라 움직일 위치를 보내주는게 좋음
+            // stream.SendNext(e_SpriteState);
+            // stream.SendNext(b_Fired);
+            // stream.SendNext(v_MousePos);
         }
         else
         {
             // Network player, receive data
-            SpriteState _e_SpriteState = (SpriteState)stream.ReceiveNext();
-            bool _b_Fired = (bool)stream.ReceiveNext();
-            Vector3 _v_MousePos = (Vector3)stream.ReceiveNext();
-            
-            // below value should be setted by manually
-            AnimationControl(_e_SpriteState, _b_Fired);
-            RotateGun(_v_MousePos);
+            v_NetworkPosition = (Vector3)stream.ReceiveNext();
+            // SpriteState _e_SpriteState = (SpriteState)stream.ReceiveNext();
+            // bool _b_Fired = (bool)stream.ReceiveNext();
+            // Vector3 _v_MousePos = (Vector3)stream.ReceiveNext();
 
-            // timestamp: info.timestamp
+            // Todo: RotateGun, AnimationControl 함수 지우고 Update에서 Network Trigger
+            // RotateGun(_v_MousePos);
+            // AnimationControl(_e_SpriteState, _b_Fired);
+            f_LastNetworkDataReceivedTime = info.timestamp;
         }
     }
 
@@ -233,6 +259,6 @@ public class Player : CharacterGeneral
 
     protected override void OnTriggerEnter2D(Collider2D col)
     {
-        
+
     }
 }
