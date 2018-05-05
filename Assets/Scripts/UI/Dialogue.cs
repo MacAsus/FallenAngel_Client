a2811c510ded28f64ff5ff4361d42f41c8c0319d
Yoohogyun <yoohoogun114@naver.com>
2018-05-05 09:13:24 +0900

    Dialogue Stash
    Dialogue Completed

--- Assets/Scripts/UI/Dialogue.cs
+++ Assets/Scripts/UI/Dialogue.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
 public class Dialogue  {
+	public Dialogue(string _name, string[] _sentences) {
+        name = _name;
+        sentences = _sentences;
+    }
 	public string name;
 	[TextArea(3, 10)]
 	public string[] sentences;
