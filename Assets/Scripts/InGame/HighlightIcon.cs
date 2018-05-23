eb0376d8d01593c77e4f211e3a1375e57d87ca26
Yoohogyun <yoohoogun114@naver.com>
2018-05-23 17:40:56 +0900

    Disabled default modal

---
+++ Assets/Scripts/InGame/HighlightIcon.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighlightIcon : MonoBehaviour
+{
+    private Canvas canvas;
+
+    [SerializeField]
+    private PhotonVoiceRecorder recorder;
+
+    [SerializeField]
+    private PhotonVoiceSpeaker speaker;
+
+    [SerializeField]
+    private Image recorderSprite;
+
+    [SerializeField]
+    private Image speakerSprite;
+
+    [SerializeField]
+    private Text bufferLagText;
+
+    private bool showSpeakerLag;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    private void Awake()
+    {
+        canvas = GetComponent<Canvas>();
+        if (canvas != null && canvas.worldCamera == null) { canvas.worldCamera = Camera.main; }
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        recorderSprite.enabled = recorder != null && recorder.IsTransmitting &&
+                PhotonVoiceNetwork.ClientState == ExitGames.Client.Photon.LoadBalancing.ClientState.Joined;
+        speakerSprite.enabled = speaker != null && speaker.IsPlaying &&
+                PhotonVoiceNetwork.ClientState == ExitGames.Client.Photon.LoadBalancing.ClientState.Joined;
+        bufferLagText.enabled = showSpeakerLag && speaker.IsPlaying && speaker.IsVoiceLinked;
+        bufferLagText.text = string.Format("{0}", speaker.CurrentBufferLag);
+    }
+
+    private void LateUpdate()
+    {
+        if (canvas == null || canvas.worldCamera == null) { return; } // should not happen, throw error
+        transform.rotation = Quaternion.Euler(0f, canvas.worldCamera.transform.eulerAngles.y, 0f); //canvas.worldCamera.transform.rotation;
+    }
+
+
+}
