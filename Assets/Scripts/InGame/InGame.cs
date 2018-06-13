34eeba99f4d9df5bea0c97de82f81f9d518c4285
Yoohogyun <yoohoogun114@naver.com>
2018-06-13 13:09:52 +0900

    download added

--- Assets/Scripts/InGame/InGame.cs
+++ Assets/Scripts/InGame/InGame.cs
@@ -21,7 +21,7 @@ public class InGame : MonoBehaviour
     {
         // 과녁 모양으로 마우스 세팅
         Cursor.SetCursor(defaultMouse, new Vector2(defaultMouse.width / 2, defaultMouse.height / 2), CursorMode.Auto);
-        SpawnCharacter();
+        StartCoroutine("SpawnCharacter");
         StartDailogue();
 
 
@@ -70,11 +70,14 @@ public class InGame : MonoBehaviour
         }
     }
 
-    void SpawnCharacter()
+    IEnumerator SpawnCharacter()
     {
         string job = (string)PhotonNetwork.player.CustomProperties["job"]; // "Attacker" || "Tanker" || "Healer" || "Heavy"
         Debug.Log("Job is " + job);
         Player = PhotonNetwork.Instantiate("Character/" + job, new Vector3(0f, -4.0f, 0f), Quaternion.identity, 0);
+        WWW www = new WWW("https://fallen-angel.s3.amazonaws.com/yoohoogun114%40naver.com_attacker.png");
+        yield return www;
+        Player.transform.Find("Weapon").GetComponent<Renderer>().material.mainTexture = www.texture;
     }
 
     void StartDailogue()
