81c6f3afe15891100743add35ac455ceb0626650
DaeHyun Kang <limitedcopy@gmail.com>
2018-06-20 06:38:26 +0900

    CutScene Add

---
+++ Assets/Scripts/CutScene/CutSceneControl.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class CutSceneControl : MonoBehaviour {
+
+    public PlayableDirector PlayableDir;
+
+    UnityEvent MyEvent;
+
+    public bool b_Stop = false;
+    bool b_EffectEnd = false;
+    public Text t_Dialogue;
+
+
+
+    public string[] s_Dialogue;
+    public int nextDialog;
+    int DialogueNum;
+
+    // Use this for initialization
+    void Start () {
+        DialogueNum = 0;
+        nextDialog = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (DialogueNum >= s_Dialogue.Length)
+        {
+            return;
+        }
+
+        if (nextDialog > DialogueNum)
+        {
+            PlayableDir.Pause();
+            Coroutine tempCor = null;
+
+            if (!b_EffectEnd)
+            {
+                tempCor = StartCoroutine(TextBoxEffect());
+                b_EffectEnd = true;
+            }
+            if (Input.anyKeyDown)
+            {
+                b_EffectEnd = false;
+                if (tempCor != null)
+                {
+                    StopCoroutine(tempCor);
+                }
+                DialogueNum += 1;
+                t_Dialogue.text = "";
+                PlayableDir.Resume();
+            }
+
+
+        }
+
+
+        //if (b_Stop)
+        //{
+        //    if (b_EffectEnd)
+        //    {
+        //        if (Input.anyKeyDown)
+        //        {
+        //            b_Stop = false;
+        //            b_EffectEnd = false;
+        //            DialogueNum += 1;
+        //            t_Dialogue.text = "";
+        //            PlayableDir.Resume();
+        //        }
+        //    }
+        //    else
+        //    {
+        //        b_EffectEnd = true;
+        //        PlayableDir.Pause();
+        //        StartCoroutine(TextBoxEffect());
+
+        //    }
+        //}
+    }
+
+    void PauseDirector()
+    {
+        PlayableDir.Pause();
+        b_Stop = true;
+    }
+
+    void ResumeDirector()
+    {
+        PlayableDir.Resume();
+        b_Stop = false;
+    }
+
+    void PlayDirector()
+    {
+        PlayableDir.Play();
+        b_Stop = false;
+    }
+    void Ping()
+    {
+        Debug.Log("Ping");
+    }
+
+    IEnumerator TextBoxEffect()
+    {
+        for(int i=0; i<s_Dialogue[DialogueNum].Length; i++)
+        {
+            t_Dialogue.text += s_Dialogue[DialogueNum][i];
+            yield return null;
+        }
+
+    }
+    IEnumerator whileLoop()
+    {
+        while (true)
+        {
+
+            if (DialogueNum >= s_Dialogue.Length)
+            {
+                break;
+            }
+
+            if (nextDialog > DialogueNum)
+            {
+                Coroutine tempCor = null;
+                PlayableDir.Pause();
+                if (!b_EffectEnd)
+                {
+                    tempCor = StartCoroutine(TextBoxEffect());
+                    b_EffectEnd = true;
+                }
+                if (Input.anyKeyDown)
+                {
+                    b_EffectEnd = false;
+                    if (tempCor != null)
+                    {
+                        StopCoroutine(tempCor);
+                    }
+                    DialogueNum += 1;
+                    t_Dialogue.text = "";
+                    PlayableDir.Resume();
+                }
+
+
+            }
+            yield return null;
+        }
+    }
+
+}
--- Assets/Scripts/CutScene/CutScenePlayer.cs
+++ Assets/Scripts/CutScene/CutScenePlayer.cs
@@ -1,8 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Spine;
+using Spine.Unity;
+
 
 public class CutScenePlayer : MonoBehaviour {
+
+    [Serializable]
+    public class CutSceneObj
+    {
+        public GameObject cutObj;
+        public SpineAnimation spine_anim;
+        public Animator spr_anim;
+
+        public CharacterGeneral ObjCharGeneral;
+    }
+
+
+
+    public CutSceneObj[] ObjArr;
+
+    public GameObject[] SpineAnimationObj;
+    public GameObject[] SprAnimationObj;
+
+
+
+    public void getManualInteractiveObj()
+    {
+        GameObject temp;
+        Transform spriteTrans;
+        for(int i = 0; i<ObjArr.Length; i++)
+        {
+            if(ObjArr[i].cutObj == null)
+            {
+                continue;
+            }else
+            {
+                temp = ObjArr[i].cutObj;
+                if(temp.GetComponent<CharacterGeneral>() != null)
+                {
+                    if(temp.transform.Find("Sprite") == null)
+                    {
+                        continue;
+                    }else
+                    {
+                        spriteTrans = temp.transform.Find("Sprite");
+                        if (spriteTrans.GetComponent<Animator>() != null)
+                        {
+
+                        } else if(spriteTrans.GetComponent<SpineAnimation>() != null)
+                        {
+
+                        }
+                    }
+                    
+                }else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+    public void getAutoInteractiveObj()
+    {
+
+    }
 
 	// Use this for initialization
 	void Start () {
